b847f9f8064c1f4d4d4160d84eb44555eab89da1
tolgaustu <67876465+tolgaustu@users.noreply.github.com>
2021-03-14 23:56:08 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,7 @@
-#Explain your work
+cv1 = {'isim':'Tolga','soyisim':'Uştu','tel':'55','diploma':'link'}
+cv2 = {'isim':'Ahmet','soyisim':'Arsu','tel':'55','diploma':'link'}
+cv3 = {'isim':'Mehmet','soyisim':'Denizcan','tel':'55','diploma':'link'}
+cv4 = {'isim':'Yılmaz','soyisim':'Güllü','tel':'55','diploma':'link'}
+cv5 = {'isim':'Burak','soyisim':'Koç','tel':'55','diploma':'link'}
 
-#Question 1
-for x in range(a):
-	print(a)
+print(cv1,"\n",cv2,"\n",cv3,"\n",cv4,"\n",cv5)
