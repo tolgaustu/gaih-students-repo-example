a72773c3986e1b4e9da05fde130d5a1ff8d606e5
tolgaustu <67876465+tolgaustu@users.noreply.github.com>
2021-03-14 23:55:57 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,16 @@
-#Explain your work
+list1 = []
+list2 = []
 
-#Question 1
-for x in range(a):
-	print(a)
+for i in range(10):
+    if i % 2:
+        list1.append(i)
+    else:
+        list2.append(i)
+
+print(list1)
+print(list2)
+mergedList = list1 + list2
+mergedList.sort()
+for i in mergedList:
+    mergedList[i] = i * 2
+    print("{}. index: {}".format(i, type(mergedList[i])))
